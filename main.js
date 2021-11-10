a87b2d925742a3a8b9a0d2faece9eccb803d9876
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2021-11-10 17:42:08 -0500

    added dependencies, edited main.js file

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
 	width: 1280,
 	height: 720,
   })
+  
   win.loadFile('index.html')
   win.webContents.on('new-window', (e, url) => {
     e.preventDefault()
@@ -13,13 +14,24 @@ const createWindow = () => {
 }
 
 app.on('ready', async () => {
-    createWindow()
-    try {
-      await session.defaultSession.loadExtension(path.join(os.homedir(), /* path to extension */))
-    } catch (err) {
-      console.log(err)
+  createWindow()
+  try {
+    if (process.platform === 'darwin'){
+      await session.defaultSession.loadExtension(path.join(os.homedir(), '/Library/Application Support/Google/Chrome/Default/Extensions/pbmlfaiicoikhdbjagjbglnbfcbcojpj/1.7.17_0/'))
+    } else if (process.platform === 'win32') {
+      await session.defaultSession.loadExtension("%LOCALAPPDATA%\\Google\\Chrome\\User Data\\Default\\Extensions\\pbmlfaiicoikhdbjagjbglnbfcbcojpj\\1.7.17_0\\")
     }
-  })
+  } catch(e) {
+    // console.log(e)
+    if (Notification.isSupported()) {
+      const notify = new Notification({title: 'Error Loading Simplify Gmail Extension', body: 'Click this notification to install Simplify Gmail extension'})
+      notify.show()
+      notify.on('click', () => require('electron').shell.openExternal('https://chrome.google.com/webstore/detail/simplify-gmail/pbmlfaiicoikhdbjagjbglnbfcbcojpj'))
+    } else {
+      console.log(e)
+    }
+  }
+})
 
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
