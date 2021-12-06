8ed57f5de35f79a33b0758978a72e0410e89895c
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2021-12-06 00:27:01 -0500

    Update nodeMailer.js

--- src/helpers/nodeMailer.js
+++ src/helpers/nodeMailer.js
@@ -9,34 +9,32 @@ let sendMail = async function(Subject, Recipient, Body)
     var myRecipient = Recipient;
     var myBody = Body;
 
-    // create reusable transporter object using the default SMTP transport
+
     let transporter = Nodemailer.createTransport({
       host: "smtp.ethereal.email",
       port: 587,
-      secure: false, // true for 465, false for other ports
+      secure: false, 
       auth: {
         user: testAccount.user, // generated ethereal user
         pass: testAccount.pass, // generated ethereal password
       },
     });
   
-    // send mail with defined transport object
     let info = await transporter.sendMail(
       {
-      from: testAccount.user, // sender address
-      to: myRecipient, // list of receivers
-      subject: mySubject, // Subject line
-      text: myBody, // plain text body
+      from: testAccount.user, 
+      to: myRecipient, 
+      subject: mySubject, 
+      text: myBody, 
     });
   
     
   
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
     
-    // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", Nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
   }
   
 
