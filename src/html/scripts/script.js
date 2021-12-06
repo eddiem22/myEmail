553a2bdd59c1ea1fe17c37ccb0f17c4ba4a6928f
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2021-12-05 21:25:05 -0500

    Update script.js

--- src/html/scripts/script.js
+++ src/html/scripts/script.js
@@ -4,8 +4,6 @@ var form =  document.getElementById('ipcForm')
 console.log
        form.addEventListener('submit', async function(event) {
         //event.preventDefault()
-       //var inputs = document.getElementById("emailform").elements
-       console.log("do something you piece of shit")
        var myRecipient = document.getElementById('email').value
        var mySubject = document.getElementById('subject').value
        var myMessage = document.getElementById('message').value
