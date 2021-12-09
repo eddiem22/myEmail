b0c909dcc1946ac8becb08c708ce81fb4e0405b7
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2021-12-09 12:27:53 -0500

    Update nodeMailer.js

--- src/helpers/nodeMailer.js
+++ src/helpers/nodeMailer.js
@@ -1,28 +1,32 @@
 let Nodemailer = require('nodemailer');
 const { app, BrowserWindow, ipcMain, net, remote, ipcRenderer} = require('electron');
 const { indexOf } = require('lodash');
-let sendMail = async function(Subject, Recipient, Body) 
+let sendMail = async function(Subject, Recipient, Body, Email, Password, Host) 
 {
     let testAccount = await Nodemailer.createTestAccount();
 
     var mySubject = Subject;
     var myRecipient = Recipient;
     var myBody = Body;
+    var myEmail = Email;
+    var myPassword = Password;
+    var myHost = Host;
 
 
     let transporter = Nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
+      host: `smtp.mail.${myHost}.com`,
+      port: 465,
+      service:`${myHost}`,
       secure: false, 
       auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
+        user: `${myEmail}`, 
+        pass: `${myPassword}`, 
       },
     });
   
     let info = await transporter.sendMail(
       {
-      from: testAccount.user, 
+      from: `${myEmail}`, 
       to: myRecipient, 
       subject: mySubject, 
       text: myBody, 
