ce9986f6c2b6702a1d3be1b75cb103095d3cd393
Eduard Marcencov <90940827+eddiem22@users.noreply.github.com>
2022-04-02 00:08:40 -0400

    Update script.js

--- src/html/scripts/script.js
+++ src/html/scripts/script.js
@@ -84,7 +84,7 @@ form.addEventListener('submit', async function(event) {
        console.log(emailObject)
        
         //console.log(inputs);
-        ipcRenderer.send('send_email', mySubjectValue, emailObject.subject.value, emailObject.message.value, myEmailValue,emailObject.password.value , emailObject.host.value )
+        ipcRenderer.send('send_email', emailObject.subject.value, emailObject.message.value, myEmailValue,emailObject.password.value , emailObject.host.value )
 
 })
   
